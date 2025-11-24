4dad146303b55e97e19e9c6f2dd31759dd47af8f
Mislav Rukonic <mislavrukonic@gmail.com>
2025-11-24 17:15:41 +0100

    remove phd related info

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,9 +5,7 @@ function About() {
         <section id="about" className="about-section">
             <h2>About Me</h2>
             <h3>
-                Master of Science in Engineering (Software Engineering). Currently applying to smart-city
-                oriented PhD positions, specifically in Estonia (hybrid, willing to commute when required from Croatia).
-                Alongside this, looking for work in Software Engineering. Open to hybrid/on-site work.
+                Master of Science in Engineering (Software Engineering). Looking for work in Software Engineering. Open to hybrid/on-site work.
             </h3>
         </section>
     );
--- src/data/interests.js
+++ src/data/interests.js
@@ -1,5 +1,5 @@
 const interests = [
-    { id: 1, name: "Smart city design and engineering (looking to do my PhD in this field!)" },
+    { id: 1, name: "Smart city design and engineering" },
     { id: 2, name: "C" },
     { id: 3, name: "Embedded and Real-Time Systems" },
     { id: 4, name: "RTOS" },
