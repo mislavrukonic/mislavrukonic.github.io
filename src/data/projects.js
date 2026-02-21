c091bdd4bc231b672d655c85c28dde2107322edd
Mislav Rukonic <mislavrukonic@gmail.com>
2026-02-21 10:37:38 +0100

    update file paths for deploy

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,22 +1,26 @@
+import rimacPhoto from '../assets/rimacrace.png'
+import fitwayLogo from '../assets/fitway-04-04 (1).png'
+import ttGameJam from '../assets/taltech_gc.png'
+
 const projects = [
     {
         id: 1,
         title: "FitWayHR",
-        image: "src/assets/fitway-04-04 (1).png",
+        image: `${fitwayLogo}`,
         description: "Fitness website desinged for a small fitness studio in Zagreb, Croatia",
         link: "https://www.fitwayhr.com/"
     },
     {
         id: 2,
         title: "Rimac RACE",
-        image: "src/assets/rimacrace.png",
+        image: `${rimacPhoto}`,
         description: "Backend Lead at Bugatti Rimac for Bachelor's Thesis (more info found on my LinkedIn)",
         link: "https://www.linkedin.com/in/mislavrukonic/#projects"
     },
     {
         id: 3,
         title: "Level Designer and Jack of All Trades at TalTech GameJam 2024",
-        image: "https://scontent-sjc3-1.xx.fbcdn.net/v/t39.30808-6/470217930_122190314282182299_1691726733536722367_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=75d36f&_nc_ohc=uQQB1mVWvlEQ7kNvwEVaPiO&_nc_oc=AdmBYP-v_6cicww5EogQPk57G9ZR3KWZ2xeHz6qKIx0pSRG9QoM0HPz4sZbGfCxumRo&_nc_zt=23&_nc_ht=scontent-sjc3-1.xx&_nc_gid=_-mfSmemkN0YX4w2pTPbdQ&oh=00_AfeL20usQv91O8N35WRk_GGusIZ_K66jGXqrKPXCkYUPQA&oe=68FF5F39",
+        image: `${ttGameJam}`,
         description: "Konnrad's Unusual Breakfast. Made with Unity.",
         link: "https://lucijaf.itch.io/konnrads-unusual-breakfast"
     },
