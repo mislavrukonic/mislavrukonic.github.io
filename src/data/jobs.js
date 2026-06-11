b794983cede415cb1a0453d8fcad38e762f5eaba
Mislav Rukonic <mislavrukonic@gmail.com>
2026-06-12 00:52:31 +0200

    add new position

--- src/data/jobs.js
+++ src/data/jobs.js
@@ -1,6 +1,15 @@
 const jobs = [
     {
         id: 1,
+        title: "Direct Sales Representative",
+        company: "Duka interijeri projekt d.o.o.",
+        typeOfJob: "Part-time",
+        duration: "June 2026 - Present",
+        location: "Zagreb, Croatia",
+        description: "Assisting with SEO, sales, and new product selection for a variety of clients.",
+    },
+    {
+        id: 2,
         title: "Fullstack developer at aiKATE",
         company: "aiKATE",
         typeOfJob: "Contract",
@@ -9,7 +18,7 @@ const jobs = [
         description: "Created and maintained fullstack software for a brand new mobile app for Vendlog. React-Native, NodeJS, and mariaDB.\nComplete overhaul of company website, designed with Astro and CSS.\nOptimized and created new features for main cosnumer webapp with NestJS.\nDocumented and tested numerous REST APIs from various vendors using Swagger.",
     },
     {
-        id: 2,
+        id: 3,
         title: "Database and Backend Engineer",
         company: "MindChip OÜ",
         typeOfJob: "Internship",
@@ -18,7 +27,7 @@ const jobs = [
         description: "Developed an auto-delete script running as a Cron job. Properly created and configured new database tables, keys, relationships, etc. Created a backend script in Node.js for the auto-deletion. Configured with proper atomicity and tested as a GET request in Postman. Integrated the script with Winston logging features. Dockerized the entire script (used Docker to create a microservice).",
     },
     {
-        id: 3,
+        id: 4,
         title: "Fullstack Developer - Web Team and Back End Engineer",
         company: "Revuto OÜ",
         typeOfJob: "Full-time",
@@ -27,7 +36,7 @@ const jobs = [
         description: "Built internal customer support tooling in Rails/MySQL; reduced average response time by 52%. Automated generation of business metric reports.",
     },
     {
-        id: 4,
+        id: 5,
         title: "Tutor for Data Structures and Algorithm Analysis",
         company: "Rochester Institute of Technology",
         typeOfJob: "Contract",
